94ea2cbd1814761150d72f8f21f105120f78076d
Dylaann <c00204772@itcarlow.ie>
2017-02-17 12:04:47 +0000

    Lab11

---
+++ SFMLOpenGL/Game.h
@@ -0,0 +1,44 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/wglew.h>
+
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML\Graphics.hpp>
+
+#include <Debug.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+using namespace std;
+using namespace sf;
+using namespace glm;
+
+class Game
+{
+public:
+	Game();
+	Game(sf::ContextSettings settings);
+	~Game();
+	void run();
+private:
+	//Window window;
+	RenderWindow renderWindow;
+	View mainView;
+	bool isRunning = false;
+	void initialize();
+	void update();
+	void render();
+	void unload();
+
+	float turnSpeed = 0.05f;
+};
+
+#endif
